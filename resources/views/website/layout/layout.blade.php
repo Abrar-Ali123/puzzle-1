ea83549c8f15d9052749f346c59a6cd01c7c1a94
Abrar-Ali123 <Abrar-70@outlook.sa>
2022-09-02 21:12:39 +0200

    initial commit

--- resources/views/website/layout/layout.blade.php
+++ resources/views/website/layout/layout.blade.php
@@ -170,8 +170,7 @@
             &copy; <a class="font-weight-bold" href="#">Your Site Name</a>. All Rights Reserved.
 
             <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-            Designed by <a class="font-weight-bold" href="https://htmlcodex.com">HTML Codex</a>
-        </p>
+        
     </div>
     <!-- Footer End -->
 
